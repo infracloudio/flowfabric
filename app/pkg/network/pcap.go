9841d946cf881b811b8786aeb85f2c04b04e3fd2
Yusuf Kanchwala <yusuf@infracloud.io>
2019-10-01 14:44:28 +0530

    Add function to capture network traffic

---
+++ app/pkg/network/pcap.go
@@ -0,0 +1,76 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/infracloudio/flowfabric/app/pkg/k8s"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	snapshotlen int32 = 1024
+	promiscuous       = false
+	err         error
+	timeout     time.Duration = 30 * time.Second
+	handle      *pcap.Handle
+
+	// Will reuse these for each packet
+	ethLayer layers.Ethernet
+	ipLayer  layers.IPv4
+	tcpLayer layers.TCP
+)
+
+// Capture captures network traffic and adds pod info into the network
+// traffic output
+func Capture(iface string) {
+
+	// Open device
+	handle, err = pcap.OpenLive(iface, snapshotlen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		parser := gopacket.NewDecodingLayerParser(
+			layers.LayerTypeEthernet,
+			&ethLayer,
+			&ipLayer,
+			&tcpLayer,
+		)
+		foundLayerTypes := []gopacket.LayerType{}
+
+		err := parser.DecodeLayers(packet.Data(), &foundLayerTypes)
+		if err != nil {
+			fmt.Println("Trouble decoding layers: ", err)
+		}
+
+		for _, layerType := range foundLayerTypes {
+			if layerType == layers.LayerTypeIPv4 {
+
+				srcVal := ipLayer.SrcIP.String()
+				dstVal := ipLayer.DstIP.String()
+
+				// Add pod info
+				if _, ok := k8s.IPPodMap[srcVal]; ok {
+					srcVal = k8s.IPPodMap[srcVal]["Name"]
+				}
+
+				if _, ok := k8s.IPPodMap[dstVal]; ok {
+					dstVal = k8s.IPPodMap[dstVal]["Name"]
+				}
+
+				fmt.Println("IPv4: ", srcVal, "->", dstVal)
+			}
+			if layerType == layers.LayerTypeTCP {
+				fmt.Println("TCP Port: ", tcpLayer.SrcPort, "->", tcpLayer.DstPort)
+				fmt.Println("TCP SYN:", tcpLayer.SYN, " | ACK:", tcpLayer.ACK)
+			}
+		}
+	}
+}
